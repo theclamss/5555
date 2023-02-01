8923cfe97312a0a5efefb1285df5c17372261801
theclamss <65466543+theclamss@users.noreply.github.com>
2023-02-01 14:15:09 +0100

    Back end 0.0.1

---
+++ src/main/java/pl/server/model/Product.java
@@ -0,0 +1,36 @@
+package pl.server.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private BigDecimal price;
+
+    @Size(min = 10, max = 500)
+    private String description;
+
+    private String fileName;
+
+    @ManyToOne
+    @JsonIgnore
+    private Category category;
+
+}
---
+++ src/main/java/pl/server/service/LocalStorageService.java
@@ -0,0 +1,64 @@
+package pl.server.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Service
+public class LocalStorageService {
+
+    @Value("${localStoragePath}")
+    private String localStoragePath;
+
+    public String uploadFile(MultipartFile multipartFile) {
+        String fileURL = "";
+        try {
+            File file = convertMultipartFileToFile(multipartFile);
+            String fileName = multipartFile.getOriginalFilename();
+            fileURL = localStoragePath + "/" + fileName;
+            uploadFileToLocalStorage(fileName, file);
+            file.delete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return fileURL;
+    }
+
+    private File convertMultipartFileToFile(MultipartFile file) throws IOException {
+        File convertedFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convertedFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convertedFile;
+    }
+
+    private void uploadFileToLocalStorage(String fileName, File file) throws IOException {
+        File localStorageFile = new File(localStoragePath + "/" + fileName);
+        FileOutputStream fos = new FileOutputStream(localStorageFile);
+        fos.write(file.getPath().getBytes());
+        fos.close();
+    }
+
+    public byte[] downloadFileFromLocalStorage(String fileName) throws IOException {
+        File localStorageFile = new File(localStoragePath + "/" + fileName);
+        FileInputStream fis = new FileInputStream(localStorageFile);
+        byte[] fileBytes = new byte[(int) localStorageFile.length()];
+        fis.read(fileBytes);
+        fis.close();
+        return fileBytes;
+    }
+
+    public String deleteFileFromLocalStorage(String fileName) {
+        File localStorageFile = new File(localStoragePath + "/" + fileName);
+        if (localStorageFile.delete()) {
+            return "Deletion Successful";
+        } else {
+            return "Deletion Failed";
+        }
+    }
+}
